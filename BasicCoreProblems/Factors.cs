162764c129c66adcac0282b166111ab9ae613f0a
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-21 21:02:14 +0530

    Factors Problem

---
+++ BasicCoreProblems/Factors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class Factors
+    {
+        public static void factors()
+        {
+            Console.Write("Enter the number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n % 2 == 0)
+            {
+                Console.Write(2 + " ");
+                n /= 2;
+            }
+
+            // n must be odd at this point. So we can
+            // skip one element (Note i = i +2)
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                // While i divides n, print i and divide n
+                while (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                    n /= i;
+                }
+            }
+
+            // This condition is to handle the case when
+            // n is a prime number greater than 2
+            if (n > 2)
+                Console.Write(n);
+        }
+    }
+}
--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("(1. Flip coin  (2. Leap year  (3. Power of 2  (4. Harmonic Progression");
+            Console.WriteLine("(1. Flip coin  (2. Leap year  (3. Power of 2  (4. Harmonic Progression  (5. Factors");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -16,6 +16,8 @@
                 case 3: PowerOF2.powerof2();
                     break;
                 case 4: HarmonicProgression.Hp();
+                    break;
+                case 5: Factors.factors();
                     break;
 
             }
