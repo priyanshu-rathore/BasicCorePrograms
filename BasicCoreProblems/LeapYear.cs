5a2864fe7cc71fc51f06c44427cfd353b96118e4
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-21 17:43:00 +0530

    Leap Year program

--- BasicCoreProblems/LeapYear.cs
+++ BasicCoreProblems/LeapYear.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace BasicCoreProblems
 {
     internal class LeapYear
     {
+        public static void leapYear()
+        {
+            Console.Write("Enter year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if(year > 999 && year < 10000)
+            {
+                if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                {
+                    Console.WriteLine("Year is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine("Year is not a leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error Enter 4 digit number only");
+            }
+        }
     }
 }
--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -4,12 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Flip coin");
+            Console.WriteLine("1. Flip coin  2. Leap year");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
             {
                 case 1: FlipCoin.flipCoin();
+                    break;
+                case 2: LeapYear.leapYear();
                     break;
             }
         }
